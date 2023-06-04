804650dc8b0944a94236abbbe76f52512da86af3
AndresCarrasquillag <j.carrasquillag@uniandes.edu.co>
2023-06-03 23:19:57 -0500

    ahora si funciona

--- src/Pasarelas/PasarelaMercadoPago.java
+++ src/Pasarelas/PasarelaMercadoPago.java
@@ -9,20 +9,22 @@ import java.util.regex.Pattern;
 public class PasarelaMercadoPago implements PasarelaDePago {
 	@Override
     public boolean hacerPago(String nombreCliente, String correoCliente, Double valor, String tarjetaDeCredito, String cvv, String fecha){
+        boolean a = validarStrings(correoCliente, cvv, tarjetaDeCredito, nombreCliente);
         if (tarjetaDeCredito.length()!=16 || cvv.length()!=3){
             return false;
+        } else if (a==false) {
+            return false;
         }
-        String linea = nombreCliente+";"+correoCliente+";"+Double.toString(valor)+";"+tarjetaDeCredito+";"+cvv+";"+fecha+"\n";
+        String linea = nombreCliente+";"+correoCliente+";"+Double.toString(valor)+";"+tarjetaDeCredito+";"+cvv+";"+fecha;
         generarRegistro(linea);
-        
-        return validarStrings(correoCliente, cvv, tarjetaDeCredito, nombreCliente);
+        return true;
     }
     
     public boolean validarStrings(String correo, String cvv, String tarjeta, String nombre) {
-        String regex = "^[ A-Za-z0-9+_.-]+@(.+)$";
+        String regex = "^[A-Za-z0-9+_.-]+@(.+)$";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(correo);
-        if (cvv.matches("\\d+") && matcher.matches() && tarjeta.matches("\\d+") && nombre.matches("^[a-zA-Z]+$")) {
+        if (cvv.matches("\\d+") && matcher.matches() && tarjeta.matches("\\d+") && nombre.matches("^[a-zA-Z ]+$")) {
         	return true;
     	}
         return false;
